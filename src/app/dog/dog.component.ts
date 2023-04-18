8bce26cafdfff4c4eb82a9eae23c722689f7da4e
Nicolas <nilucio65@gmail.com>
2023-04-18 11:26:47 -0300

    first commit

---
+++ src/app/dog/dog.component.ts
@@ -0,0 +1,32 @@
+import { DogService } from './../dog.service';
+import { Dog } from './../dog';
+import { Component, OnInit } from '@angular/core';
+import { getLocaleDayNames } from '@angular/common';
+
+@Component({
+  selector: 'app-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.css']
+})
+export class DogComponent implements OnInit {
+
+  constructor(private DogService: DogService) {}
+
+  ngOnInit(): void {
+    this.loadDog();
+  }
+  loadDog() {
+  this.DogService.getRandomDog().subscribe({
+    next : dog => this.dog = dog
+  });
+  }
+
+  dog: Dog = {
+
+
+  } as Dog;
+
+  getName(): string[]{
+    return this.dog.message.split("/")[4].split("-");
+  }
+}
